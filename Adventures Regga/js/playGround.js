f4f95386ff8ae931cced6634791bbe9e0c5b206f
dabbe01 <dabbe01@bredband.net>
2022-06-25 11:44:23 +0200

    playground update

--- Adventures Regga/js/playGround.js
+++ Adventures Regga/js/playGround.js
@@ -1,100 +1,213 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
 
-let myGameCaracter
-let myGameHouse
-let myGameHouseDoor
-let myGameEnemy
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height)
 
 
-function startGame() {
-    myGameCaracter = new component(20, 20, "blue", 320, 300);
-    myGameHouse = new component(100, 100, "red", 290, 40);
-    myGameHouseDoor = new component(20, 30, "black", 330, 110);
-    myGameEnemy = new component(20, 20, "green", 500, 200);
-    myGameArea.start();
-}
 
-var myGameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 700;
-        this.canvas.height = 350;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20)
-        window.addEventListener('keydown', function (e) {
-            myGameArea.keys = (myGameArea.keys || []);
-            myGameArea.keys[e.keyCode] = true;
-          })
-          window.addEventListener('keyup', function (e) {
-            myGameArea.keys[e.keyCode] = false;
-          })
-    },
-    clear : function(){
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-    },
-    stop : function() {
-        clearInterval(this.interval);
-      }
-    
-}
-
-function component(width, height, color, x, y){
-    this.gamearea = myGameArea
-    this.width = width
+class Sprite {
+  constructor({position, velocity, color = 'blue', width, height}) {
+    this.position = position
+    this.velocity = velocity
+    this.width = width 
     this.height = height
-    this.speedX = 0
-    this.speedY = 0
-    this.x = x
-    this.y = y
+    this.color = color
+  
+  }
 
-    this.update = function(){
-    ctx = myGameArea.context
-    ctx.fillStyle = color
-    ctx.fillRect(this.x, this.y, this.width, this.height)
+  draw(){
+    c.fillStyle = this.color
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
 
-    }
-    this.newPos = function(){
-        this.x += this.speedX
-        this.y += this.speedY
-    }
-
-    this.crashWith = function(otherobj) {
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
-        if ((mybottom < othertop) ||
-        (mytop > otherbottom) ||
-        (myright < otherleft) ||
-        (myleft > otherright)) {
-          crash = false;
-        }
-        return crash;
-      }
-}
-
-function updateGameArea(){
-    if (myGameCaracter.crashWith(myGameEnemy)) {
-        myGameArea.stop();
-      } else {
-          myGameArea.clear()
-          myGameHouse.update();
-          myGameHouseDoor.update()
-          myGameEnemy.update()
-          myGameCaracter.speedX = 0
-          myGameCaracter.speedY = 0
-          if (myGameArea.keys && myGameArea.keys[37]) {myGameCaracter.speedX = -2; }
-          if (myGameArea.keys && myGameArea.keys[39]) {myGameCaracter.speedX = 2; }
-          if (myGameArea.keys && myGameArea.keys[38]) {myGameCaracter.speedY = -2; }
-          if (myGameArea.keys && myGameArea.keys[40]) {myGameCaracter.speedY = 2; }
-          myGameCaracter.newPos();    
-          myGameCaracter.update()
-      }
+  update(){
+    this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
     
+   
+  }
+
+  }
+
+const player = new Sprite({
+  position: {
+    x: 30,
+    y: 30    
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  },
+  width: 50,
+  height: 50
+  
+
+})
+
+
+const enemy = new Sprite({
+  position: {
+    x: 800,
+    y: 350   
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  },
+  width: 50,
+  height: 50,
+  color: 'red'
+})
+
+const wall = new Sprite({
+  position: {
+    x: 110,
+    y: 0    
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  },
+  width: 50,
+  height: 300,
+  color: 'white'
+  
+
+})
+const wall2 = new Sprite({
+  position: {
+    x: 250,
+    y: 278   
+  },
+  velocity: {
+    x: 0,
+    y: 0
+  },
+  width: 50,
+  height: 300,
+  color: 'white'
+  
+
+})
+
+
+
+
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  w: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  }
 }
+
+let lastKey
+
+function animate(){
+  window.requestAnimationFrame(animate)
+  c.fillStyle = 'black'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  player.update()
+  enemy.update()
+  wall.update()
+  wall2.update()
+ 
+
+  player.velocity.x = 0
+  player.velocity.y = 0
+  if (keys.a.pressed && lastKey === 'a'){
+    player.velocity.x = -1
+  }else if (keys.d.pressed && lastKey === 'd'){
+    player.velocity.x = 1
+  }else if (keys.w.pressed && lastKey === 'w'){
+    player.velocity.y = -1
+  }else if (keys.s.pressed && lastKey === 's'){
+    player.velocity.y = 1
+  }
+
+  if (player.position.x + player.width >= wall.position.x && 
+    player.position.x <= wall.position.x + wall.width &&
+    player.position.y + player.height >= wall.position.y &&
+    player.position.y <= wall.position.y + wall.height
+    ){
+        wall.color = 'green'
+    }else{
+      wall.color = 'white'
+    }
+
+ 
+  // if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x 
+  //   && player.attackBox.position.x <= enemy.position.x + enemy.width
+  //   && player.attackBox.position.y + player.attackBox.height >= enemy.position.y
+  //   && player.attackBox.position.y <= enemy.position.y + enemy.height 
+  //   && player.isAttacking
+  
+  //   ){
+  //     player.isAttacking = false
+  //     enemy.health -= 20
+  //     document.querySelector('#enemyHealth').style.width = enemy.health + '%'
+  //   console.log('test')
+  // }
+
+  // if (enemy.health <= 0 || player.health <= 0){
+  //   document.querySelector('#displayText').innerHTML = 'You won!!'
+  //   document.querySelector('#displayText').style.display = 'flex'
+  //   enemy.position.x = -100
+  // }
+}
+
+animate()
+
+window.addEventListener('keydown', (event) =>{
+  switch (event.key) {
+    case 'd':
+    keys.d.pressed = true
+    lastKey = 'd'
+    break
+    case 'a':
+      keys.a.pressed = true
+      lastKey = 'a'
+    break
+    case 'w':
+        keys.w.pressed = true
+        lastKey = 'w'
+    break
+    case 's':
+          keys.s.pressed = true
+          lastKey = 's'
+      break
+   
+    
+  }
+  
+})
+
+window.addEventListener('keyup', (event) =>{
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false
+    break
+    case 'a':
+      keys.a.pressed = false
+      break
+     case 'w':
+        keys.w.pressed = false
+      break
+      case 's':
+        keys.s.pressed = false
+      break
+  }
+  
+})
 
