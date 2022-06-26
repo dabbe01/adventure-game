7a6aa9543a7191430ab5b98d9951e37176a38cbc
dabbe01 <dabbe01@bredband.net>
2022-06-26 23:43:47 +0200

    sprite update

--- Adventures Regga/js/playGround.js
+++ Adventures Regga/js/playGround.js
@@ -1,213 +1,162 @@
+import platform from '../img/platform.png'
+console.log(platform)
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = innerWidth
+canvas.height = innerHeight
 
-c.fillRect(0, 0, canvas.width, canvas.height)
-
-
-
-class Sprite {
-  constructor({position, velocity, color = 'blue', width, height}) {
-    this.position = position
-    this.velocity = velocity
-    this.width = width 
-    this.height = height
-    this.color = color
-  
+const gravity = 1.5
+class Player {
+  constructor(){
+    this.position = {
+      x: 100,
+      y: 100
+    }
+    this.velocity ={
+      x: 0,
+      y: 0
+    }
+    this.width = 30
+    this.height = 30
   }
 
   draw(){
-    c.fillStyle = this.color
+    c.fillStyle = 'red'
     c.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
 
   update(){
     this.draw()
-    this.position.x += this.velocity.x
     this.position.y += this.velocity.y
-    
-   
+    this.position.x += this.velocity.x
+
+    if(this.position.y + this.height + 
+      this.velocity.y <= canvas.height)
+      this.velocity.y += gravity
+    else this.velocity.y = 0
+
+    }
+}
+
+class Platform {
+  constructor({x, y}){
+    this.position = {
+      x,
+      y
+    }
+    this.width = 200
+    this.height = 20
   }
 
+  draw(){
+    c.fillStyle = 'blue'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
-
-const player = new Sprite({
-  position: {
-    x: 30,
-    y: 30    
-  },
-  velocity: {
-    x: 0,
-    y: 0
-  },
-  width: 50,
-  height: 50
-  
-
-})
+}
 
 
-const enemy = new Sprite({
-  position: {
-    x: 800,
-    y: 350   
-  },
-  velocity: {
-    x: 0,
-    y: 0
-  },
-  width: 50,
-  height: 50,
-  color: 'red'
-})
-
-const wall = new Sprite({
-  position: {
-    x: 110,
-    y: 0    
-  },
-  velocity: {
-    x: 0,
-    y: 0
-  },
-  width: 50,
-  height: 300,
-  color: 'white'
-  
-
-})
-const wall2 = new Sprite({
-  position: {
-    x: 250,
-    y: 278   
-  },
-  velocity: {
-    x: 0,
-    y: 0
-  },
-  width: 50,
-  height: 300,
-  color: 'white'
-  
-
-})
-
-
-
+const player = new Player()
+const platforms = [new Platform({x: 200, y: 100}), 
+  new Platform({x: 500, y: 200})]
 
 const keys = {
-  a: {
+  right:{
     pressed: false
   },
-  d: {
-    pressed: false
-  },
-  w: {
-    pressed: false
-  },
-  s: {
+  left: {
     pressed: false
   }
 }
 
-let lastKey
+let scrollOffset = 0
 
 function animate(){
-  window.requestAnimationFrame(animate)
-  c.fillStyle = 'black'
-  c.fillRect(0, 0, canvas.width, canvas.height)
+  requestAnimationFrame(animate)
+  c.clearRect(0, 0, canvas.width, canvas.height)
   player.update()
-  enemy.update()
-  wall.update()
-  wall2.update()
- 
+  platforms.forEach((platform) => {
+    platform.draw()
 
-  player.velocity.x = 0
-  player.velocity.y = 0
-  if (keys.a.pressed && lastKey === 'a'){
-    player.velocity.x = -1
-  }else if (keys.d.pressed && lastKey === 'd'){
-    player.velocity.x = 1
-  }else if (keys.w.pressed && lastKey === 'w'){
-    player.velocity.y = -1
-  }else if (keys.s.pressed && lastKey === 's'){
-    player.velocity.y = 1
+  })
+
+  if(keys.right.pressed && player.position.x < 450){
+    player.velocity.x = 5
+  } else if (keys.left.pressed && player.position.x > 100){
+    player.velocity.x = -5
+  }else{
+    player.velocity.x = 0
   }
 
-  if (player.position.x + player.width >= wall.position.x && 
-    player.position.x <= wall.position.x + wall.width &&
-    player.position.y + player.height >= wall.position.y &&
-    player.position.y <= wall.position.y + wall.height
-    ){
-        wall.color = 'green'
-    }else{
-      wall.color = 'white'
+  if(keys.right.pressed){
+    scrollOffset += 5
+    platforms.forEach((platform) => {
+      platform.draw()
+      platform.position.x -= 5
+      })
+  } else if (keys.left.pressed){
+    scrollOffset -= 5
+    platforms.forEach((platform) => {
+      platform.draw()
+      platform.position.x += 5
+    })
+  }
+    
+
+  // player collision detection
+  platforms.forEach((platform) => {
+    platform.draw()
+  if(player.position.y + player.height 
+    <= platform.position.y && 
+    player.position.y + player.height +
+    player.velocity.y >= platform.position.y &&
+    player.position.x + player.width >= 
+    platform.position.x && player.position.x 
+     <= platform.position.x + platform.width){
+      player.velocity.y = 0
     }
+  })
 
- 
-  // if (player.attackBox.position.x + player.attackBox.width >= enemy.position.x 
-  //   && player.attackBox.position.x <= enemy.position.x + enemy.width
-  //   && player.attackBox.position.y + player.attackBox.height >= enemy.position.y
-  //   && player.attackBox.position.y <= enemy.position.y + enemy.height 
-  //   && player.isAttacking
-  
-  //   ){
-  //     player.isAttacking = false
-  //     enemy.health -= 20
-  //     document.querySelector('#enemyHealth').style.width = enemy.health + '%'
-  //   console.log('test')
-  // }
+  if(scrollOffset > 2000){
+    console.log('you best')
+  }
+  }
 
-  // if (enemy.health <= 0 || player.health <= 0){
-  //   document.querySelector('#displayText').innerHTML = 'You won!!'
-  //   document.querySelector('#displayText').style.display = 'flex'
-  //   enemy.position.x = -100
-  // }
-}
 
 animate()
 
-window.addEventListener('keydown', (event) =>{
-  switch (event.key) {
-    case 'd':
-    keys.d.pressed = true
-    lastKey = 'd'
-    break
+addEventListener('keydown', ({key}) => {
+  switch(key){
     case 'a':
-      keys.a.pressed = true
-      lastKey = 'a'
-    break
-    case 'w':
-        keys.w.pressed = true
-        lastKey = 'w'
+      keys.left.pressed = true
     break
     case 's':
-          keys.s.pressed = true
-          lastKey = 's'
-      break
-   
-    
-  }
-  
-})
-
-window.addEventListener('keyup', (event) =>{
-  switch (event.key) {
-    case 'd':
-      keys.d.pressed = false
+      console.log('down')
     break
-    case 'a':
-      keys.a.pressed = false
-      break
-     case 'w':
-        keys.w.pressed = false
-      break
-      case 's':
-        keys.s.pressed = false
-      break
+    case 'd':
+    keys.right.pressed = true
+    break
+    case 'w':
+      player.velocity.y -= 20
+    break
   }
-  
+  console.log(keys.right.pressed)
 })
 
+addEventListener('keyup', ({key}) => {
+  switch(key){
+    case 'a':
+      keys.left.pressed = false
+    break
+    case 's':
+      console.log('down')
+    break
+    case 'd':
+      keys.right.pressed = false
+    break
+    case 'w':
+      player.velocity.y -= 20
+    break
+  }
+  console.log(keys.right.pressed)
+})
